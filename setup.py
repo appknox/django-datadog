dcc860f5d4248339b4ae4f3c5c79494aabc02d68
dhilipsiva <dhilipsiva@gmail.com>
2016-02-23 10:33:43 +0530

    Fix setup and deploy scripts

--- setup.py
+++ setup.py
@@ -9,19 +9,21 @@ if [sys.version_info[0], sys.version_info[1]] < [2, 7]:
     reqs.append("simplejson>=2.0.9")
 
 setup(
-    name = 'django-datadog',
-    version = '0.1.0',
-    packages = [
+    name='ak-django-datadog',
+    version='0.1.0.2',
+    packages=[
         'datadog',
         'datadog.middleware'
     ],
-    include_package_data = True,
-    license = 'BSD',
-    description = ' simple Django middleware for submitting timings and exceptions to Datadog.',
-    long_description = README,
-    author = 'Conor Branagan',
-    author_email = 'conor.branagan@gmail.com',
-    install_requires = reqs.extend([
+    include_package_data=True,
+    license='BSD',
+    description=(
+        'Simple Django middleware for submitting timings and exceptions to'
+        ' Datadog.'),
+    long_description=README,
+    author='Conor Branagan',
+    author_email='conor.branagan@gmail.com',
+    install_requires=reqs.extend([
         'dogapi==1.2.1'
     ])
 )
